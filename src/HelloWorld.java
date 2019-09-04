fadfd1b7e2f81f571b64e0a366dea16b961018e3
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-04 11:30:04 -0400

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is September 04, 2019. I am alive!");
+        System.out.println("My name is Daniel");
+    }
+}
